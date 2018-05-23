9a843369add6c43401f7bd549be929b1b8ae73ce
tnkathy <39344196+tnkathy@users.noreply.github.com>
2018-05-23 16:15:33 -0500

    Xamarin
    This includes the Xamarin Form cross-platform files. There's an additional Android set of files "ghAPI.Android" that was too large for me to upload. I didn't do any changes in there so I don't think it's necessary for me to include it anyway. But let me know if you need to see the Android files.

---
+++ Xamarin/ghAPI/PublicTab.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ghAPI
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PublicTab : ContentPage
+	{
+		public PublicTab ()
+		{
+			InitializeComponent ();
+            getPublicEvents();
+        }
+
+        private async void getPublicEvents() 
+        {
+
+            HttpClient client = new HttpClient();
+
+            try
+            {
+                var response = await client.GetStringAsync("https://api.github.com/events");
+                var pubEvents = JsonConvert.DeserializeObject<PublicUser.Root>(response);
+                publicEventsListView.ItemsSource = pubEvents.ToString();
+
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine(e.ToString());
+            }
+
+            //try
+            //{
+            //    using (WebClient client = new WebClient())
+            //    {
+            //        //string url = string.Format("https://api.github.com/events");
+            //        var response = client.DownloadString("https://api.github.com/events");
+            //        var pubEvents = JsonConvert.DeserializeObject<PublicUser.Root>(response);
+            //        publicEventsListView.ItemsSource = pubEvents.ToString();
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.Out.WriteLine(e.ToString());
+            //}
+        }
+    }
+}
---
+++ Xamarin/ghAPI/PublicUser.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace ghAPI
+{
+    class PublicUser
+    {
+        public class Actor
+        {
+            public string login { get; set; }
+            public string avatar_url { get; set; }
+        }
+
+        public class Repo
+        {
+            public string name { get; set; }
+            public string url { get; set; }
+        }
+
+        public class Root
+        {
+            public string type { get; set; }
+            public Actor actor { get; set; }
+            public Repo repo { get; set; }
+        }
+
+    }
+}
